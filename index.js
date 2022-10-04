396915fa18ad9f03a7df85cb66c63d88acebf304
Samyak Pawar <67262220+ArtistSamyak@users.noreply.github.com>
2022-10-04 11:27:33 +0200

    date counter removed

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 
-const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-let today = new Date();
-const todayDay = today.getDate();
-const todayMonth = today.getMonth();
-const todayYear = today.getFullYear();
+// const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+// let today = new Date();
+// const todayDay = today.getDate();
+// const todayMonth = today.getMonth();
+// const todayYear = today.getFullYear();
 
-today = new Date(todayYear,todayMonth,todayDay);
-const meetup = new Date(2022, 09, 02);
+// today = new Date(todayYear,todayMonth,todayDay);
+// const meetup = new Date(2022, 09, 02);
 
-const diffDays = Math.round((meetup - today) / oneDay);
-const h2 = document.querySelector("h2")
+// const diffDays = Math.round((meetup - today) / oneDay);
+// const h2 = document.querySelector("h2")
 
-if (diffDays > 1) {
-  h2.innerHTML = diffDays + " Days to go!"
-} else if (diffDays === 1) {
-  h2.innerHTML = " 1 Day to go!";
-} else if (diffDays === 0) {
-  h2.innerHTML = "🎉 Today! 🎉";
-} else {
-  h2.innerHTML = "Complete!";
-}
+// if (diffDays > 1) {
+//   h2.innerHTML = diffDays + " Days to go!"
+// } else if (diffDays === 1) {
+//   h2.innerHTML = " 1 Day to go!";
+// } else if (diffDays === 0) {
+//   h2.innerHTML = "🎉 Today! 🎉";
+// } else {
+//   h2.innerHTML = "Complete!";
+// }
